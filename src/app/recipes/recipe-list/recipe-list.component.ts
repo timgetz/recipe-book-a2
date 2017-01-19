4b371d1c6ff0b40e80ec2ce2fb63c39543331be1
Tim Getz <timgetz@gmail.com>
2017-01-19 17:09:47 -0500

    Initial Import, part 2

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit, Output} from '@angular/core';
+import {Recipe} from "../recipe";
+import {EventEmitter} from "@angular/forms/src/facade/async";
+
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html'
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+  recipe = new Recipe('Dummy', 'Dummy', 'http://thumbs2.ebaystatic.com/d/l225/m/mfXELL6zPWJE4OC0agiXMZw.jpg');
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
+}
