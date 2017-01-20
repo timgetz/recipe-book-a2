d621ec3b8daf0f4757eba8a12f4ef317380d3602
Tim Getz <timgetz@gmail.com>
2017-01-19 23:23:23 -0500

    Completed - Section 5 -Directives

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,9 +8,11 @@ import {EventEmitter} from "@angular/forms/src/facade/async";
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'http://www.dessertfortwo.com/wp-content/uploads/2014/12/Schnitzel-2.jpg',[]),
+    new Recipe('Summer Salad', 'Okayish', 'http://www.bbcgoodfood.com/sites/default/files/recipe_images/recipe-image-legacy-id--46004_11.jpg',[])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy', 'Dummy', 'http://thumbs2.ebaystatic.com/d/l225/m/mfXELL6zPWJE4OC0agiXMZw.jpg');
 
   constructor() { }
 
